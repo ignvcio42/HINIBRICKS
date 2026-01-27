822ce3f2b5f893aeb1b04048a4d5e8e21c437950
ignvcio42 <ignacio.guajardo_m@mail.udp.cl>
2026-01-27 01:02:24 -0300

    Update deployment configuration for improved stability

---
+++ src/data/Lego/fondos.ts
@@ -0,0 +1,82 @@
+import { getCloudinaryUrl } from "~/lib/cloudinary";
+
+export const Fondos = [
+    {
+        id: 1,
+        name: 'Ropa 1',
+        description: 'Lego Mannequin',
+        image: getCloudinaryUrl('/Fondos/(Inglés).png'),
+    },
+    {
+        id: 2,
+        name: 'Ropa 2',
+        description: 'Lego Mannequin',
+        image: getCloudinaryUrl('/Fondos/2.png'),
+    },
+    {
+        id: 3,
+        name: 'Ropa 3',
+        description: 'Lego Mannequin',
+        image: getCloudinaryUrl('/Fondos/3.png'),
+    },
+    {
+        id: 4,
+        name: 'Ropa 4',
+        description: 'Lego Mannequin',
+        image: getCloudinaryUrl('/Fondos/4.png'),
+    },
+    {
+        id: 5,
+        name: 'Ropa 5',
+        description: 'Lego Mannequin',
+        image: getCloudinaryUrl('/Fondos/5.png'),
+    },
+    {
+        id: 6,
+        name: 'Ropa 6',
+        description: 'Lego Mannequin',
+        image: getCloudinaryUrl('/Fondos/6.png'),
+    },
+    {
+        id: 7,
+        name: 'Ropa 7',
+        description: 'Lego Mannequin',
+        image: getCloudinaryUrl('/Fondos/7.png'),
+    },
+    {
+        id: 8,
+        name: 'Ropa 8',
+        description: 'Lego Mannequin',
+        image: getCloudinaryUrl('/Fondos/8.png'),
+    },
+    {
+        id: 9,
+        name: 'Ropa 9',
+        description: 'Lego Mannequin',
+        image: getCloudinaryUrl('/Fondos/9.png'),
+    },
+    {
+        id: 10,
+        name: 'Ropa 10',
+        description: 'Lego Mannequin',
+        image: getCloudinaryUrl('/Fondos/10.png'),
+    },
+    {
+        id: 11,
+        name: 'Ropa 11',
+        description: 'Lego Mannequin',
+        image: getCloudinaryUrl('/Fondos/11.png'),
+    },
+    {
+        id: 12,
+        name: 'Ropa 12',
+        description: 'Lego Mannequin',
+        image: getCloudinaryUrl('/Fondos/12.png'),
+    },
+    {
+        id: 13,
+        name: 'Ropa 13',
+        description: 'Lego Mannequin',
+        image: getCloudinaryUrl('/Fondos/13.png'),
+    },
+]
