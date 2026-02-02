ccedddcb847a6f31849f42d709cd86e1cc43d267
hianlvrt <h.gonzlezmaluenda@uandresbello.edu>
2026-02-02 14:57:30 -0300

    HIANnombresimagenes

--- src/data/Qas.ts
+++ src/data/Qas.ts
@@ -2,11 +2,11 @@ export const Qas = [
     {
         id: 1,
         question: "¿Qué es HiniBricks?",
-        answer: "HiniBricks es una empresa que se dedica a la venta de lego, con más de 10 años de experiencia en el mercado."
+        answer: "HiniBricks es una empresa que se dedica a la venta de lego, con más de 3 años de experiencia en el mercado."
     },
     {
         id: 2,
-        question: "¿Qué productos ofrece?",
+        question: "¿Qué productos ofrecemos?",
         answer: "Ofrecemos una amplia gama de productos de lego, desde sets completos hasta piezas individuales."
     },
 ]
